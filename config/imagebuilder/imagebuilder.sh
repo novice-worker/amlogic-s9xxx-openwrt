8fe4ecf2ed863eaa898a4e5336124971286abcf9
novice-worker <cicakterbang099@gmail.com>
2025-10-29 19:13:27 +0700

    Update package list in imagebuilder.sh
    Removed and added several packages in the image builder script.

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -174,21 +174,19 @@ rebuild_firmware() {
 
     # Selecting default packages, lib, theme, app and i18n, etc.
     my_packages="\
-        acpid attr base-files bash bc blkid block-mount blockd bsdtar btrfs-progs busybox bzip2 \
-        cgi-io chattr comgt comgt-ncm containerd coremark coreutils coreutils-base64 coreutils-nohup \
-        coreutils-truncate curl docker docker-compose dockerd dosfstools dumpe2fs e2freefrag e2fsprogs \
-        exfat-mkfs f2fs-tools f2fsck fdisk gawk getopt git gzip hostapd-common iconv iw iwinfo jq \
-        jshn kmod-brcmfmac kmod-brcmutil kmod-cfg80211 kmod-mac80211 libjson-script liblucihttp \
-        liblucihttp-lua losetup lsattr lsblk lscpu mkf2fs mount-utils openssl-util parted \
-        perl-http-date perlbase-file perlbase-getopt perlbase-time perlbase-unicode perlbase-utf8 \
-        pigz ppp ppp-mod-pppoe pv rename resize2fs runc tar tini ttyd tune2fs \
-        uclient-fetch uhttpd uhttpd-mod-ubus unzip uqmi usb-modeswitch uuidgen wget-ssl whereis \
-        which wpad-basic wwan xfs-fsck xfs-mkfs xz xz-utils ziptool zoneinfo-asia zoneinfo-core zstd \
-        \
-        luci luci-base luci-compat luci-i18n-base-zh-cn luci-lib-base luci-lib-docker \
-        luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio luci-mod-admin-full luci-mod-network \
-        luci-mod-status luci-mod-system luci-proto-3g luci-proto-ipip luci-proto-ipv6 \
-        luci-proto-ncm luci-proto-openconnect luci-proto-ppp luci-proto-qmi luci-proto-relay \
+        kmod-brcmfmac kmod-gpio-button-hotplug kmod-r8168 kmod-tun \
+        kmod-usb-serial kmod-usb-net kmod-usb-serial-wwan kmod-usb-serial-option kmod-usb-net-qmi-wwan kmod-usb-net-cdc-mbim luci-proto-modemmanager \
+        libmbim libqmi \
+        coreutils coreutils-base64 coreutils-nohup curl \
+        chinadns-ng dns2socks ip-full \
+        libuci-lua lua luci-lib-jsonc \
+        microsocks resolveip tcping \
+        ipt2socks kmod-nft-socket kmod-nft-tproxy kmod-nft-nat \
+        xray-core unzip \
+        ttyd \
+        luci-base\
+        kmod-usb-core kmod-mii kmod-fs-ext4 \
+        adguardhome\
         \
         luci-app-amlogic luci-i18n-amlogic-zh-cn \
         \
